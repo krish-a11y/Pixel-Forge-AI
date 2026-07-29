721e2b22a08dab93cef57df2e9b1dbe74192d01e
krishna ghogare <mnshghogare@gmail.com>
2026-07-30 00:59:12 +0530

    clerk middleware config done

---
+++ proxy.ts
@@ -0,0 +1,50 @@
+// middleware for all endpoints
+import { auth } from "@clerk/nextjs";
+import { clerkMiddleware ,createRouteMatcher} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+// the routes that can be acces without token
+const  isPublicRoute=createRouteMatcher([
+  "/signin",
+  "/signup",
+  "/home",
+  "/"
+])
+
+// the api routes that can be acces without token
+const isPublicApiRoute=createRouteMatcher([
+ "/api/videos"
+]) 
+export default   clerkMiddleware(async(auth,req)=>
+{
+   const {userId}=await auth();
+   const currentUrl=new URL(req.url);
+  const isHomePage=currentUrl.pathname==="/home"
+
+  // if the user is logged in (has token) then he cant access the public routes except the home route
+  if(userId&&isPublicRoute(req)&&!isHomePage)
+  {
+    return  NextResponse.redirect(new URL("/home",req.url));
+  }
+  if(!userId)
+    {
+      // if the user in not logged in then he cant access any private route
+      if(!isPublicApiRoute(req)&&!isPublicRoute(req))
+        {
+      return  NextResponse.redirect(new URL("/signin",req.url));
+    }
+  }
+  // giving control to next section
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+    // Always run for Clerk-specific frontend API routes
+    "/__clerk/(.*)",
+  ],
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -23,11 +30,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
