291380f370ea394de1b12ac5ab3a9ab5b3da7851
krishna ghogare <mnshghogare@gmail.com>
2026-08-16 22:29:36 +0530

    video dowenload added on the home page

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,8 +1,66 @@
-import React from 'react'
+"use client"
+import React, { useCallback, useEffect, useState } from 'react'
 import axios from 'axios'
 import VideoCard from '../../../../components/VideoCard'
-
+import { Video } from '../../../../types';
 function Home() {
+  // treacking the videos on the site
+  const [videos,setVideos]=useState<Video[]>([]);
+  const [loading,setLoading]=useState(true);
+  const [error,setError]=useState(null);
+
+  // getting all the videos from the DB
+  const fetchVideos=useCallback(async()=>{
+
+    try {
+        const response=await axios.get("/api/videos");
+        if(Array.isArray(response.data))
+        {
+          // setting the videos to the fetched videos
+          setVideos(response.data);
+        }
+    } catch (error:any) {
+     console.log(error);
+     setError(error); 
+    }
+    // setting the loading to false as video fetch is completed
+    finally
+    {
+     setLoading(false);
+    }
+  },[])
+
+  useEffect(()=>{
+    fetchVideos();
+  },[fetchVideos])
+
+  // downloading  the video
+  const handleDownload=useCallback((title:string,url:string)=>{
+   
+      // creating new element 
+      const link=document.createElement("a");
+      // appending  the video link 
+      link.href=url;
+      // as the user click the video open on new window
+      link.setAttribute("target","_blank")
+
+      // tell the browser to download the video
+      link.setAttribute("download",`${title}.png`);
+
+      // appending the child to the DOM
+      document.body.appendChild(link);
+
+      // automatically clicking on the tag to start the doenload
+      link.click();
+
+      // removing the element as the download is over
+      document.body.removeChild(link);
+  },[])
+
+  if(loading)
+  {
+    return <div>Loading...</div>
+  }
   return (
     <div>Home</div>
   )
